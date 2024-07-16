4203d8b8bfadfc89f71d52aa033109ecce1ce6cb
naeemgg <naeemqg1@gmail.com>
2024-07-16 15:46:36 +0530

    learn adding else block

--- src/components/Conditional.svelte
+++ src/components/Conditional.svelte
@@ -1,6 +1,6 @@
 <script>
-    export let user
+    export let user="Guest"
 </script>
 
-<p>This component will render only if you're signed in</p>
+<p>This component will render only if you're {#if user==="Guest"} not {/if}signed in</p>
 <h1>Oh hello {user}!</h1>
