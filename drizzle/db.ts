d45abb20f445c21accf81c0bc496324a551fdbb7
naeemgg <naeemqg1@gmail.com>
2024-07-16 16:57:48 +0530

    how to give id while looping

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,13 +1,13 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
-import { randFullName } from '@ngneat/falso';
+// import { randFullName } from '@ngneat/falso';
 import * as schema from "./schema"
 const sqlite = new Database('sqlite.db');
-export const db = drizzle(sqlite,{schema});
+export const db = drizzle(sqlite,{schema,logger:true});
 
-(async()=>{
-    for (let i = 0; i < 11; i++) {
-        await db.insert(schema.users).values([{id:crypto.randomUUID(),name:randFullName()}])
-    }
-    console.log(await db.query.users.findMany())
-})()
+// (async()=>{
+//     for (let i = 0; i < 11; i++) {
+//         await db.insert(schema.users).values([{id:crypto.randomUUID(),name:randFullName()}])
+//     }
+//     console.log(await db.query.users.findMany())
+// })()
