da1785d418fff525379fca33370ab30de93620dc
naeemgg <naeemqg1@gmail.com>
2024-07-16 16:15:12 +0530

    map alternative in svelte is each .. as

---
+++ src/components/LoopingComponent.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import {randEmail,randColor} from "@ngneat/falso"
+    let emails:string[] = []
+    for(let i = 0;i<200;i++){
+        emails.push(randEmail())
+        emails = emails
+    }
+</script>
+
+
+
+<div class="p-4 w-full flex flex-wrap justify-between">
+    {#each emails as currentEmail}
+    <p class="p-1 border border-red-600 m-1">{currentEmail}</p>
+    {/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,6 @@
 <script>
-	import Conditional from "../components/Conditional.svelte";
-	let isLogin = false
-    
+	import LoopingComponent from "../components/LoopingComponent.svelte";
 </script>
 
 
-<div>
-    <button on:click={()=>isLogin = true} class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" >Login</button>
-    <button on:click={()=>isLogin = false} class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" >Logout</button>
-
-    {#if isLogin}
-    <Conditional user=Naeemgg secret='Sveltekit is awesome'/>
-    {:else}
-    <Conditional user=Shaun/>
-    {/if}
-    <Conditional/>
-</div>
+    <LoopingComponent/>
