6d817e945428c3a73e90539ab62721b7daac4d30
naeemgg <naeemqg1@gmail.com>
2024-07-17 18:38:37 +0530

    auto-subscribe with $varName

--- src/components/CountStore.svelte
+++ src/components/CountStore.svelte
@@ -3,12 +3,9 @@
 	import Increment from './Increment.svelte';
 	import Reset from './Reset.svelte';
 	import { count } from './store';
-
-	let insiderCount: number;
-	count.subscribe((currentValue) => (insiderCount = currentValue));
 </script>
 
-<h1>{insiderCount}</h1>
+<h1>{$count}</h1>
 <Increment />
 <Decrement />
 <Reset />
