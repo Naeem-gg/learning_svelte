0b0d562c6409c9146b3166315af5295600a66831
naeemgg <naeemqg1@gmail.com>
2024-07-17 17:07:38 +0530

    use binding with select>options

---
+++ src/components/SelectBind.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { randCity, randIp } from '@ngneat/falso';
+	let cities: { id: string; name: string }[] = [];
+	for (let i = 0; i < 10; i++) {
+		cities[i] = { id: randIp(), name: randCity() };
+	}
+	let selected = '';
+	let answer: string;
+	function handleSubmit() {
+		if (selected.startsWith(answer)) alert('Correct answer: ');
+		else alert('Wrong answer');
+	}
+</script>
+
+<h1>Select Any city and write its first 3 letters</h1>
+<div>
+	<select bind:value={selected}>
+		{#each cities as city (city.id)}
+			<option value={city.name}>{city.name}</option>
+		{/each}
+	</select>
+	<input type="text" bind:value={answer} class="border-4" />
+	<button type="button" on:click={handleSubmit} class="rounded-lg p-2 m-2 bg-black text-white"
+		>Submit</button
+	>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Checkbox from '../components/Checkbox.svelte';
+	import SelectBind from '../components/SelectBind.svelte';
 </script>
 
-<Checkbox />
+<SelectBind />
